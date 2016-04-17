11ab5e45bce303c4f2fae972cd914e4b7835110a
Gemynd AI <gemyndai@gmail.com>
2016-04-17 14:49:05 +0000

    Initial commit with Telegram API

---
+++ gemynd/core/message.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#
+# A library that provides a Gemynd AI bot interface
+# Copyright (C) 2016
+# Gemynd AI Team <devs@gemynd.ai>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+class Message:
+
+    def __init__(self, id, user_id, user_name, chat_id, text):
+        self.id = id
+        self.user_id = user_id
+        self.user_name = user_name
+        self.chat_id = chat_id
+        self.text = text
+
+    def __str__(self):
+        result = {'id': self.id,
+                  'user_id': self.user_id,
+                  'user_name': self.user_name,
+                  'chat_id': self.chat_id,
+                  'text': self.text}
+        return str(result)
